2776b2d1da052b0f25e32eda5a4744511e5b27e3
irma660 <1325455@senati.pe>
2023-08-31 17:39:07 -0500

    incorporando entrada de números

--- CALCULADORA_C#/Form1.cs
+++ CALCULADORA_C#/Form1.cs
@@ -20,11 +20,13 @@ namespace CALCULADORA_C_
         private void button5_Click(object sender, EventArgs e)
         {
             //número 5
+            tbDisplay.Text = "5";
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
             //número 3
+            tbDisplay.Text = "3";
         }
 
         private void button13_Click(object sender, EventArgs e)
@@ -40,16 +42,19 @@ namespace CALCULADORA_C_
         private void button7_Click(object sender, EventArgs e)
         {
             //número 1
+            tbDisplay.Text = "1";
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
             //número 6
+            tbDisplay.Text = "6";
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             //número 4
+            tbDisplay.Text = "4";
         }
 
         private void button10_Click(object sender, EventArgs e)
@@ -61,6 +66,7 @@ namespace CALCULADORA_C_
         private void button1_Click(object sender, EventArgs e)
         {
             //número 8
+            tbDisplay.Text = "8";
         }
 
         private void button14_Click(object sender, EventArgs e)
@@ -76,16 +82,19 @@ namespace CALCULADORA_C_
         private void button8_Click(object sender, EventArgs e)
         {
             //número 2
+            tbDisplay.Text = "2";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             //número 7
+            tbDisplay.Text = "7";
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             //número 9
+            tbDisplay.Text = "9";
         }
 
         private void button16_Click(object sender, EventArgs e)
@@ -100,9 +109,14 @@ namespace CALCULADORA_C_
 
         private void button17_Click(object sender, EventArgs e)
         {
-
+            //signoPunto
+            tbDisplay.Text = ".";
         }
 
-       
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            //boton limpiar
+
+        }
     }
 }
